885d5214f1f4a927a6a48214794aeca7f0138324
JabyBaii <bailiang0919@163.com>
2025-01-27 22:45:59 +0800

    update type

--- uibill/models.py
+++ uibill/models.py
@@ -14,24 +14,25 @@ class Transaction(models.Model):
         ('J', '出行'),
         ('K', '投资'),
         ('L', '收入'),
+        ('M', '娱乐'),
     ]
 
     PAYMENT_CHOICES = [
-        ('1', '微信'),
-        ('2', '支付宝'),
-        ('3', '现金'),
-        ('4', '京东白条'),
-        ('5', '交通银行信用卡'),
-        ('6', '农商行信用卡'),
-        ('7', '其他银行卡'),
-        ('8', '美团'),
+        ('WCP', '微信'),
+        ('ZFB', '支付宝'),
+        ('CSH', '现金'),
+        ('JDP', '京东白条'),
+        ('JTC', '交通信用卡'),
+        ('NSC', '农商信用卡'),
+        ('OTC', '储蓄银行卡'),
+        ('MTP', '美团'),
     ]
 
     transaction_date = models.DateField(verbose_name='交易日期')
     amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='金额')
     category_code = models.CharField(max_length=1, choices=CATEGORY_CHOICES, verbose_name='类别')
     description = models.CharField(max_length=200, verbose_name='描述')
-    payment_method = models.CharField(max_length=1, choices=PAYMENT_CHOICES, verbose_name='支付方式')
+    payment_method = models.CharField(max_length=3, choices=PAYMENT_CHOICES, verbose_name='支付方式')
     order_number = models.CharField(max_length=100, blank=True, null=True, verbose_name='订单编号')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='更新时间')
